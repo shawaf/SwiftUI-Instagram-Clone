3600e68528e5e7c1fa535ad7b77cf7c2675b79f6
mohamed.elshawaf <mohamed.elshawaf.1@gmail.com>
2023-09-17 17:56:07 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mohamed Elshawaf on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
